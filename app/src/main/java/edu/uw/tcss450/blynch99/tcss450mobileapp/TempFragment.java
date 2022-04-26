6dd748d2313b064d348994275a89779e5761e9ec
ikozor <ilyakozorezov@gmail.com>
2022-04-26 12:51:39 -0700

    🔗 Added data binding dependency

---
+++ app/src/main/java/edu/uw/tcss450/blynch99/tcss450mobileapp/TempFragment.java
@@ -0,0 +1,21 @@
+package edu.uw.tcss450.blynch99.tcss450mobileapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+/**
+ * create an instance of this fragment.
+ */
+public class TempFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_temp, container, false);
+    }
+}
