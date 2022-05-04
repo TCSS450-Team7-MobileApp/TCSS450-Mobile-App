6278d89f779df2b72c9efa44345c9a14b3971704
ikozor <ilyakozorezov@gmail.com>
2022-05-04 12:03:41 -0700

    📕Created Contacts

--- app/src/main/java/edu/uw/tcss450/blynch99/tcss450mobileapp/ui/message/MessageFragment.java
+++ app/src/main/java/edu/uw/tcss450/blynch99/tcss450mobileapp/ui/message/MessageFragment.java
@@ -2,10 +2,16 @@ package edu.uw.tcss450.blynch99.tcss450mobileapp.ui.message;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import edu.uw.tcss450.blynch99.tcss450mobileapp.R;
 
@@ -19,4 +25,29 @@ public class MessageFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_message, container, false);
     }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.title_bar_menu, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        navigateToContacts();
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void navigateToContacts() {
+        Navigation.findNavController(getView())
+                .navigate(MessageFragmentDirections
+                        .actionMessageFragmentToContactsFragment());
+    }
+
 }
