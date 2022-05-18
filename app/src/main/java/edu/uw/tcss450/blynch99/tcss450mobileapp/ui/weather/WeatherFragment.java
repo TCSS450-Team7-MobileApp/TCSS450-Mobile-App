4b39832eb6004d646c672005ac6e3f1ebf3cf320
lammai <lam7.2521@gmail.com>
2022-05-17 19:38:34 -0700

    ⛅ Basic Weather information without icons/locations

--- app/src/main/java/edu/uw/tcss450/blynch99/tcss450mobileapp/ui/weather/WeatherFragment.java
+++ app/src/main/java/edu/uw/tcss450/blynch99/tcss450mobileapp/ui/weather/WeatherFragment.java
@@ -1,23 +1,77 @@
 package edu.uw.tcss450.blynch99.tcss450mobileapp.ui.weather;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.json.JSONObject;
 
 import edu.uw.tcss450.blynch99.tcss450mobileapp.R;
+import edu.uw.tcss450.blynch99.tcss450mobileapp.auth.model.UserInfoViewModel;
+import edu.uw.tcss450.blynch99.tcss450mobileapp.databinding.FragmentWeatherBinding;
 
 /**
  * create an instance of this fragment.
  */
 public class WeatherFragment extends Fragment {
 
+    private static final String degreeFarenheit = "°F";
+
+    private WeatherViewModel mModel;
+//    private UserInfoViewModel mUserModel;
+    private FragmentWeatherBinding mBinding;
+
+    public WeatherFragment() {}
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mModel = new ViewModelProvider(getActivity()).get(WeatherViewModel.class);
+//        mUserModel = new ViewModelProvider(getActivity()).get(UserInfoViewModel.class);
+        mModel.connectGet();
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_weather, container, false);
+//        return inflater.inflate(R.layout.fragment_weather, container, false);
+        mBinding = FragmentWeatherBinding.inflate(inflater);
+        return mBinding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Fragment parent = getParentFragment();
+        mBinding = FragmentWeatherBinding.bind(getView());
+        mModel.addResponseObserver(getViewLifecycleOwner(), this::observeResponse);
+    }
+
+    private void observeResponse(final JSONObject response) {
+        if (response.has("code")) {
+            Log.e("WEATHER REQUEST ERROR",  response.toString());
+        } else if ( response.length() != 0) {
+            setViewComponents();
+        } else {
+            Log.e("RECEIVED NO RESPONSE", response.toString());
+        }
+    }
+    private void setViewComponents() {
+        mBinding.currentTemp.setText(mModel.getmCurrentWeatherData().getmTemperature() + degreeFarenheit);
+        mBinding.weatherDescription.setText(mModel.getmCurrentWeatherData().getmDescription());
+        mBinding.feelsLike.setText("Feels like: " + mModel.getmCurrentWeatherData().getmFeelsLike() + degreeFarenheit);
+        mBinding.minTemp.setText("Min: " + mModel.getmCurrentWeatherData().getmMinTemperature() + degreeFarenheit);
+        mBinding.maxTemp.setText("Max: " + mModel.getmCurrentWeatherData().getmMaxTemperature() + degreeFarenheit);
+        mBinding.hourlyList.setAdapter(new WeatherHourlyRecyclerViewAdapter(mModel.getmHourlyForecast()));
+        mBinding.dailyList.setAdapter(new WeatherDailyRecyclerViewAdapter(mModel.getmDailyForecast()));
     }
 }
---
+++ app/src/main/java/edu/uw/tcss450/blynch99/tcss450mobileapp/ui/weather/WeatherViewModel.java
@@ -0,0 +1,173 @@
+package edu.uw.tcss450.blynch99.tcss450mobileapp.ui.weather;
+
+import android.app.Application;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LifecycleOwner;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Observer;
+
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class WeatherViewModel extends AndroidViewModel {
+
+    private final MutableLiveData<JSONObject> mResponse;
+
+    private WeatherCurrent mCurrentWeatherData;
+
+    private List<Weather> mHourlyForecast;
+    private List<Weather> mDailyForecast;
+
+    public WeatherViewModel(@NonNull Application application) {
+        super(application);
+        mResponse = new MutableLiveData<>();
+        mResponse.setValue(new JSONObject());
+        mHourlyForecast = new ArrayList<>();
+        mDailyForecast = new ArrayList<>();
+    }
+
+    public void addResponseObserver(@NonNull final LifecycleOwner theOwner,
+                                    @NonNull final Observer<? super JSONObject> theObserver) {
+        mResponse.observe(theOwner, theObserver);
+    }
+
+    public WeatherCurrent getmCurrentWeatherData() {
+        return mCurrentWeatherData;
+    }
+
+    public List<Weather> getmHourlyForecast() {
+        return mHourlyForecast;
+    }
+
+    public List<Weather> getmDailyForecast() {
+        return mDailyForecast;
+    }
+
+    public void clearResponse() {
+        mResponse.setValue(new JSONObject());
+    }
+
+    public void connectGet() {
+        // 47.246950, -122.436277
+        String tempCoords = "?lat=47.246950&lng=-122.436277";
+        String url = "https://tcss450-team7.herokuapp.com/weather" + tempCoords;
+//        String url = "http://192.168.0.13:5000/weather" + tempCoords;
+
+        Request request = new JsonObjectRequest(
+                Request.Method.GET,
+                url,
+                null,
+                this::handleResult,
+                this::handleError) {
+            @Override
+            public Map<String, String> getHeaders() {
+                Map<String, String> headers = new HashMap<>();
+                // add headers <key,value>
+//                headers.put("Authorization", theJwt);
+
+                // Production
+                headers.put("Authorization", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9." +
+                        "eyJlbWFpbCI6Imlrb3pvckB1dy5lZHUiLCJtZW1iZXJpZCI6NjUsImlhdCI6MTY1" +
+                        "MjczNzYwOCwiZXhwIjoxNjUzOTQ3MjA4fQ.xeTps5EgqSNR2oyt3erLXXixcGUWg" +
+                        "jghHiU_ogGqfrw");
+
+                // Local
+//                headers.put("Authorization",
+//                        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InBva29qNjMxMDdAaGJlaHMuY29tIiwibWVtYmVyaWQiOjcyLCJpYXQiOjE2NTI3NzAxNjIsImV4cCI6MTY1Mzk3OTc2Mn0.IucfTLvXFJwzFI1D1aJcImuGAbVrnGmgsIvlgGdBHeE"
+//                );
+                return headers;
+            }
+        };
+        request.setRetryPolicy(new DefaultRetryPolicy(
+                10_000,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        //Instantiate the RequestQueue and add the request to the queue
+        Volley.newRequestQueue(getApplication().getApplicationContext())
+                .add(request);
+    }
+
+    private void handleResult(final JSONObject theResult) {
+        WeatherCurrent currentWeatherData = null;
+        List<Weather> hourlyData = new ArrayList<>();
+        List<Weather> dailyData = new ArrayList<>();
+        try {
+            JSONObject curr = theResult.getJSONObject("currentWeather");
+            currentWeatherData = new WeatherCurrent(
+                    curr.getInt("temp"),
+                    curr.getString("description"),
+                    curr.getInt("minTemp"),
+                    curr.getInt("maxTemp"),
+                    curr.getInt("humidity"),
+                    curr.getInt("feels_like"));
+
+            JSONArray hourRes = theResult.getJSONArray("hourlyData");
+            for (int i = 0; i < hourRes.length(); i++) {
+                JSONObject hourData = (JSONObject) hourRes.get(i);
+                int hour = hourData.getInt("hours");
+                Weather newData = new Weather(
+                        i == 0 ? "Now" :
+                                (hour % 12 == 0 ? 12 : hour % 12) + (hour < 12 ? "AM" : "PM"),
+                        hourData.getInt("temp"));
+                hourlyData.add(newData);
+            }
+
+            JSONArray dailyRes = theResult.getJSONArray("dailyData");
+            for (int i = 0; i < dailyRes.length(); i++) {
+                JSONObject dayData = (JSONObject) dailyRes.get(i);
+                Weather newData = new Weather(
+                        i == 0 ? "Today" : dayData.getString("day"),
+                        dayData.getInt("temp"));
+                dailyData.add(newData);
+            }
+
+            mCurrentWeatherData = currentWeatherData;
+            mHourlyForecast = hourlyData;
+            mDailyForecast = dailyData;
+            mResponse.setValue(theResult);
+        } catch (JSONException ex) {
+            Map<String, String> map = new HashMap<>();
+            map.put("code", "JSON parse error: " + ex.getMessage());
+            mResponse.setValue(new JSONObject(map));
+            ex.printStackTrace();
+        }
+    }
+
+    /**
+     * Handles errors generated when requesting weather data from the server
+     *
+     * @param theError the resulting Volley error to be handled
+     */
+    private void handleError(final VolleyError theError) {
+        Map<String, Object> map = new HashMap<>();
+        if (theError.networkResponse != null) {
+            map.put("code", String.valueOf(theError.networkResponse.statusCode));
+            try {
+                String data = new String(theError.networkResponse.data, Charset.defaultCharset())
+                        .replace('\"', '\'');
+                map.put("data", theError.networkResponse.data == null ? new JSONObject() :
+                        new JSONObject(data));
+            } catch (JSONException ex) {
+                Log.e("JSON PARSE ERROR IN ERROR HANDLER", ex.getMessage());
+            }
+        }
+        mResponse.setValue(new JSONObject(map));
+    }
+
+}
